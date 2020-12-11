6af304877d30b629fe11ec19857e46355e38aa5a
Shubham Jain <shubjain440@gmail.com>
2020-12-11 22:13:25 +0530

    Added end-points for read and post contact

--- app/api/contacts.rb
+++ app/api/contacts.rb
@@ -4,11 +4,32 @@ class Contacts < Api
 
 	namespace :contacts, desc: 'Bags related CRUD operations' do
 
-		desc 'Contacts'
-		get '/' do
-      {status: true, message: "Yeah, you are here"}
+		desc 'Get all Contacts'
+    get '/' do
+      @contacts = Contact.all
+      if @contacts
+        { data: @contacts, status: true }
+      else
+        error!({ status: false, message: 'Contact list is empty' }, 400)
+      end
 		end
 
+    desc 'Create new contact'
+    params do
+      requires :first_name, type: String, desc: 'First name'
+      requires :last_name, type: String, desc: 'Last name'
+      requires :email, type: String, desc: 'Email'
+      requires :phone, type: String, desc: 'Phone'
+      requires :message, type: String, desc: 'Message'
+    end
+    post '/' do
+      @contact = Contact.new(params)
+      if @contact.save!
+        { data: @contact, message: 'Contact was successfully added.', status: true}
+      else
+        error!({ status: false, message: @contact.errors.full_messages.join(', ') }, 400)
+      end
+		end
 	
 
 	
