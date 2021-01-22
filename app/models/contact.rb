d8552716f7f8c9f4e2882bfa59f366ded2005b6a
Shubham Jain <shubjain440@gmail.com>
2021-01-22 17:18:16 +0530

    Add validation for data and send mail logic

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,14 @@
 class Contact < ApplicationRecord
+
+  validates :first_name, :presence => true, format: {with: /[a-zA-Z]/}
+  validates :last_name, :presence => true, format: {with: /[a-zA-Z]/}
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+  validates :phone, :presence => true,
+                    :numericality => true,
+                    :length => { :minimum => 5, :maximum => 15 }
+
+  def send_mail
+    ContactMailer.new_contact_email(self).deliver_now
+  end
+                    
 end
