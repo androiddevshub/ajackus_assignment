9d60f95f62baae0ff0fe08a7e5bb00b77cfd6240
Shubham Jain <shubjain440@gmail.com>
2021-01-22 17:20:16 +0530

    Test case and coverage updated

--- spec/requests/form_submissions_spec.rb
+++ spec/requests/form_submissions_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "form submission enpoints", :type => :request do
 
   it 'GET test endpoint returns to test code true' do
-    get '/api/contacts/test'
+    get '/api/contacts/test_endpoint'
     expect(JSON.parse(response.body)['status_code']).to eq(true)
   end
 
@@ -15,14 +15,15 @@ describe "form submission enpoints", :type => :request do
 
 
   it 'POST /api/contacts returns status code as true' do
-    post '/api/contacts', :params => { first_name: "hey maah", last_name: "man", email: "heymaah@man.com", phone: "5656787890", message: "Hey maah man, its been too long we met" }
+    post '/api/contacts', :params => { user: {first_name: "Jack", last_name: "mortan", email: "jackmortan@gmail.com", phone: "5656787890", message: "Hey Mortan, its been too long we met"} }
     expect(JSON.parse(response.body)['status_code']).to eq(true)
   end
 
   it 'POST /api/contacts returns error' do
-    post '/api/contacts', :params => { first_name: "hey maah", last_name: "man", email: "heymaah@man.com"}
-    expect(JSON.parse(response.body)['error']).to eq("phone is missing, message is missing")
+    post '/api/contacts', :params => { user: {first_name: "Jack", last_name: "Mortan", email: "jackmortan@gmail.com"}}
+    expect(JSON.parse(response.body)['error']).to eq("user[phone] is missing, user[message] is missing")
   end
+
 
 end
   
